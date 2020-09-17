9197a1fdb7c3c6c3918a2a92e386c41a98298c26
Mark Heckler <mark.heckler@gmail.com>
2020-09-17 10:15:56 -0500

    Updates

--- src/main/java/com/thehecklers/rsocketclient/RSComponent.java
+++ src/main/java/com/thehecklers/rsocketclient/RSComponent.java
@@ -2,9 +2,11 @@ package com.thehecklers.rsocketclient;
 
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import javax.annotation.PostConstruct;
+import java.time.Duration;
 
 @Component
 public class RSComponent {
@@ -14,11 +16,28 @@ public class RSComponent {
         this.requester = builder.connectTcp("localhost", 9999).block();
     }
 
-    @PostConstruct
+/*    @PostConstruct
     void doThis() {
         Mono<Pong> stringMono = requester.route("pingpong")
                 .data(new Ping("Pinging RSocket server..."))
                 .retrieveMono(Pong.class);
         stringMono.subscribe(r -> System.out.println("   >>> Returning pong: " + r.getMessage()));
+    }*/
+
+/*    @PostConstruct
+    void doThat() {
+        requester.route("pingpongflux")
+                .data(Flux.interval(Duration.ofSeconds(1))
+                        .map(l-> new Ping("Pinging RSocket server...")))
+                .retrieveFlux(Pong.class)
+                .subscribe(r -> System.out.println("   >>> Returning pong: " + r.getMessage()));
+    }*/
+
+    @PostConstruct
+    void doTheOtherThing() {
+        requester.route("onetomany")
+                .data(new Ping("Pinging RSocket server..."))
+                .retrieveFlux(Pong.class)
+                .subscribe(r -> System.out.println("   >>> Returning pong: " + r.getMessage()));
     }
 }
--- src/main/java/com/thehecklers/rsocketclient/RSConfig.java
+++ src/main/java/com/thehecklers/rsocketclient/RSConfig.java
@@ -2,6 +2,7 @@ package com.thehecklers.rsocketclient;
 
 import io.rsocket.RSocket;
 import io.rsocket.RSocketFactory;
+import io.rsocket.core.RSocketConnector;
 import io.rsocket.frame.decoder.PayloadDecoder;
 import io.rsocket.transport.netty.client.TcpClientTransport;
 import org.springframework.context.annotation.Bean;
@@ -12,13 +13,25 @@ import java.net.InetSocketAddress;
 
 @Configuration
 public class RSConfig {
-    @Bean
+//    @Bean
     RSocket rSocket() {
+/*
+        From RSocketFactory source:
+        @deprecated please use {@link RSocketConnector} and {@link RSocketServer}.
+
         return RSocketFactory.connect()
                 .dataMimeType(MimeTypeUtils.APPLICATION_JSON_VALUE)
                 .frameDecoder(PayloadDecoder.ZERO_COPY)
                 .transport(TcpClientTransport.create(new InetSocketAddress("127.0.0.1", 9999)))
                 .start()
+                .block();
+*/
+
+        // MH: Replacing above with this:
+        return RSocketConnector.create()
+                .dataMimeType(MimeTypeUtils.APPLICATION_JSON_VALUE)
+                .payloadDecoder(PayloadDecoder.ZERO_COPY)
+                .connect(TcpClientTransport.create(new InetSocketAddress("127.0.0.1", 9999)))
                 .block();
     }
 }
